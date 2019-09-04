523fb3478922a7fbae581e7ba0d9c940cd8175c1
jussi <jussi.kuokkanen@protonmail.com>
2019-09-04 22:54:09 +0300

    works

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,10 +1,8 @@
 #include "MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
-	//ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 }
 
 MainWindow::~MainWindow() {
-	delete ui;
 }
 
